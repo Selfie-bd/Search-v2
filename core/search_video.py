72b3984d106a9e6b8aaa2f1066afaad46163120e
Selfie SD <webtamil0@gmail.com>
2022-05-21 13:48:23 +0530

    Update search_video.py

--- core/search_video.py
+++ core/search_video.py
@@ -7,7 +7,7 @@ from core.get_cookies import (
     get_cookies,
     set_cookies
 )
-from core.login import doodstream_login
+from core.login import pdisk_login
 
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"
 search_api = "https://doodapi.com/api/search/videos?key=-1&item_id=&title={}&pageSize=10&pageNo=1&desc=&status=&sortField=ctime&sortAsc=0&needDirName=true"
